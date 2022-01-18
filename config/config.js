85b3e251e7c89e14d5e03478f407fd8585645057
tictactoe23 <97976034+tictactoe23@users.noreply.github.com>
2022-01-19 01:05:26 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  TOKEN: "",
-  CHAT_ID: "",
+  TOKEN: "5037280889:AAF6ax007Zbt1izosndgjS7Z5D7e7G_tMGc",
+  CHAT_ID: -666552128,
   PORT: "8080",
   database: {
     port: "3306",
     user: "gen_user",
     host: "85.193.81.243",
     database: "default_db",
-    password: "k5crowage",
+    password: "xqmhyjm0n",
   },
 };
